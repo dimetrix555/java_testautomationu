f468162498f8cfff6a563dcb55d935339884624a
dimetrix555 <dimetrix55@gmail.com>
2023-01-26 18:16:25 +0100

    Chapter 9c: Overriding and Overloading Methods

--- src/main/java/chapter9/InheritanceTester.java
+++ src/main/java/chapter9/InheritanceTester.java
@@ -2,6 +2,10 @@ package chapter9;
 
 public class InheritanceTester {
     public static void main(String[] args) {
-        Employee employee = new Employee();
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("square");
     }
 }
